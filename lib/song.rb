ceded70808053ca151c5129b33d3e8d6da7f081f
Alethia Quintero <alethiaq22@gmail.com>
2019-07-23 19:21:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
 attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
-
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -11,6 +11,7 @@ attr_accessor :name, :artist, :genre
     @genre = genre
     @@count += 1
     @@genres << @genre
+    @@artists << @artist 
   end
 
   def self.count
@@ -20,5 +21,8 @@ attr_accessor :name, :artist, :genre
   def self.genres
     @@genres.uniq
 end
+
+  def self.artists
+    @@artists.uniq
 
 end
